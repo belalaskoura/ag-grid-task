bc1408a0e8b9420ea196af1b7f733c176387810b
belalaskoura <belal.askoura@pixelogicmedia.com>
2026-07-15 10:17:27 +0300

    comments

--- src/components/GenderDropdown.jsx
+++ src/components/GenderDropdown.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-
+// Custom dropdown component for gender selection in the ag-grid
 export function GenderDropdown(props){
     const[value,setValue] = useState(props.value)
-
+    // Function to handle changes in the dropdown selection
     function handleChange(e){
         setValue(e.target.value)
-        props.node.setDataValue(props.colDef.field, e.target.value)
+        props.node.setDataValue(props.colDef.field, e.target.value) // Update the grid data with the new value
     }
     return(
         <select value={value} onChange={handleChange}>
