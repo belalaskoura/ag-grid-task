5b07cb9f5f1ceec2660de426c81dd22f38658146
belalaskoura <belal.askoura@pixelogicmedia.com>
2026-07-15 15:26:00 +0300

    Coverted ApiSlice.js to ApiSlice.ts and defined Character interface

--- src/features/apiSlice.ts
+++ src/features/apiSlice.ts
@@ -1,10 +1,26 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
+export interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+        name: string;
+        url: string;
+    };
+    location: {
+        name: string;
+        url: string;
+    };
+}
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl: 'https://rickandmortyapi.com/api/'}),
     endpoints: (builder) => ({
-        getAllCharacters: builder.query({
+        getAllCharacters: builder.query<Character[], void>({
             query: () => 'character',
         })
     })
