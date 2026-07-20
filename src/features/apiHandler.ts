034bfb4eba30bdb491b4e112410cb434de7336c6
belalaskoura <belal.askoura@pixelogicmedia.com>
2026-07-20 17:03:13 +0300

    test: Added api handler to prepare for RTK query testing

---
+++ src/features/apiHandler.ts
@@ -0,0 +1,25 @@
+import {http, HttpResponse} from 'msw'
+import {setupServer} from 'msw/node'
+
+export const mockResponse =
+    {
+        id: 1,
+        name: "Rick Sanchez",
+        status: 'Alive',
+        species: 'Human',
+        gender: 'Male',
+        location:{name: 'Citadel of Ricks'}
+
+    };
+
+export const apiHandler = [
+    http.get('https://rickandmortyapi.com/api/character', () => {
+        return HttpResponse.json({
+            results: mockResponse,
+            info: {count: 1, pages: 1, next: null, prev: null}
+        })
+    })
+]
+
+export const server = setupServer(...apiHandler)
+
