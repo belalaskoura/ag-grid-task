d97e287c1c888f41789970185d5fd8991d8c9a74
belalaskoura <belal.askoura@pixelogicmedia.com>
2026-07-21 14:23:38 +0300

    style: Add basic stying to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/Home/Home";
 import {CharacterGrid} from "./pages/CharacterGrid";
 export function App(){
   return (
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,12 @@
 import {useNavigate} from "react-router-dom";
+import './Home.css'
 
 export function Home() {
     const navigate = useNavigate();
     return (
-        <div>
-            <h1>Welcome to the Rick and Morty App</h1>
-            <div>
+        <div className="home">
+            <h1 >Welcome to the Rick and Morty App</h1>
                 <button onClick={() => navigate('/grid')}>View Characters</button>
-            </div>
         </div>        
     )   
 }
